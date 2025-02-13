16b9af16f232d2a3df889f69978eca860b02d4a7
damar <damar@gitlab.lab>
2025-02-13 15:15:43 +0700

    build 13/2/2025 15:15

--- browser/chunk-CVDKI3HB.js
+++
@@ -1,1 +0,0 @@
-var n=class{noEmployee;fullName;userName;directorateId;divisionId;departmentId;unitId;positionId;levelId;status;projectMasterList=[];projectId;startEmpDate;endEmpDate};export{n as a};
